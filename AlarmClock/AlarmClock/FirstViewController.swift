faa42796c27db1eb0c598081683808ecd6c2923e
200213257 <200213257@ba-a251-05.student.georgianc.on.ca>
2015-03-26 17:27:35 -0400

    add the base core defaults to the git repo to start building the app

---
+++ AlarmClock/AlarmClock/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AlarmClock
+//
+//  Created by 200213257 on 2015-03-26.
+//  Copyright (c) 2015 200213257. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AlarmClock/AlarmClock/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AlarmClock
+//
+//  Created by 200213257 on 2015-03-26.
+//  Copyright (c) 2015 200213257. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
